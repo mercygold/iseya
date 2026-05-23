b19987af25733de23a38a412ceb3473155241986
Anuoluwapo Joshua <mercygold@Anuoluwapos-MacBook-Air.local>
2026-05-23 03:38:20 -0700

    Fix production Tailwind stylesheet configuration

---
+++ tailwind.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+const config = {
+  content: [
+    "./app/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./lib/**/*.{ts,tsx}",
+  ],
+};
+
+export default config;
